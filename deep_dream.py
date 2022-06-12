e3a9df57364e4df823373f759646b448a0719c5d
SteampunkIslande <charles.monod-broca@neuf.fr>
2022-06-12 18:50:07 +0200

    Made it work without deprecated pytorch methods
    And fixed argument parser issues

--- deep_dream.py
+++ deep_dream.py
@@ -10,39 +10,45 @@ import os
 import tqdm
 import scipy.ndimage as nd
 from utils import deprocess, preprocess, clip
+from torchvision.transforms import ToPILImage
 
 
 def dream(image, model, iterations, lr):
-    """ Updates the image to maximize outputs for n iterations """
-    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available else torch.FloatTensor
-    image = Variable(Tensor(image), requires_grad=True)
+    """Updates the image to maximize outputs for n iterations"""
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    image = torch.Tensor(image).requires_grad_(True).to(device)
+    image.retain_grad()
     for i in range(iterations):
         model.zero_grad()
         out = model(image)
-        loss = out.norm()
+        loss = torch.linalg.norm(out)
         loss.backward()
         avg_grad = np.abs(image.grad.data.cpu().numpy()).mean()
         norm_lr = lr / avg_grad
         image.data += norm_lr * image.grad.data
         image.data = clip(image.data)
         image.grad.data.zero_()
-    return image.cpu().data.numpy()
+    return image.cpu().detach().numpy()
 
 
-def deep_dream(image, model, iterations, lr, octave_scale, num_octaves):
-    """ Main deep dream method """
-    image = preprocess(image).unsqueeze(0).cpu().data.numpy()
+def deep_dream(image: Image, model, iterations, lr, octave_scale, num_octaves):
+    """Main deep dream method"""
+    image = preprocess(image).unsqueeze(0).cpu().numpy()
 
     # Extract image representations for each octave
     octaves = [image]
     for _ in range(num_octaves - 1):
-        octaves.append(nd.zoom(octaves[-1], (1, 1, 1 / octave_scale, 1 / octave_scale), order=1))
+        octaves.append(
+            nd.zoom(octaves[-1], (1, 1, 1 / octave_scale, 1 / octave_scale), order=1)
+        )
 
     detail = np.zeros_like(octaves[-1])
     for octave, octave_base in enumerate(tqdm.tqdm(octaves[::-1], desc="Dreaming")):
         if octave > 0:
             # Upsample detail to new octave dimension
-            detail = nd.zoom(detail, np.array(octave_base.shape) / np.array(detail.shape), order=1)
+            detail = nd.zoom(
+                detail, np.array(octave_base.shape) / np.array(detail.shape), order=1
+            )
         # Add deep dream detail from previous octave to new base
         input_image = octave_base + detail
         # Get new deep dream image
@@ -50,29 +56,47 @@ def deep_dream(image, model, iterations, lr, octave_scale, num_octaves):
         # Extract deep dream details
         detail = dreamed_image - octave_base
 
-    return deprocess(dreamed_image)
+    return dreamed_image
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_image", type=str, default="images/supermarket.jpg", help="path to input image")
-    parser.add_argument("--iterations", default=20, help="number of gradient ascent steps per octave")
-    parser.add_argument("--at_layer", default=27, type=int, help="layer at which we modify image to maximize outputs")
+    parser.add_argument(
+        "--input-image",
+        type=str,
+        default="images/supermarket.jpg",
+        help="path to input image",
+    )
+    parser.add_argument(
+        "--iterations",
+        default=20,
+        type=int,
+        help="number of gradient ascent steps per octave",
+    )
+    parser.add_argument(
+        "--at-layer",
+        default=15,
+        type=int,
+        help="layer at which we modify image to maximize outputs",
+    )
     parser.add_argument("--lr", default=0.01, help="learning rate")
-    parser.add_argument("--octave_scale", default=1.4, help="image scale between octaves")
-    parser.add_argument("--num_octaves", default=10, help="number of octaves")
+    parser.add_argument(
+        "--octave-scale", default=1.4, type=float, help="image scale between octaves"
+    )
+    parser.add_argument("--num-octaves", default=10, type=int, help="number of octaves")
     args = parser.parse_args()
 
     # Load image
-    image = Image.open(args.input_image)
+    # image = Image.open(args.input_image)
+    image = torch.rand(1500, 1000, 3).numpy()
 
     # Define the model
-    network = models.vgg19(pretrained=True)
+    network = models.vgg16(pretrained=True)
     layers = list(network.features.children())
     model = nn.Sequential(*layers[: (args.at_layer + 1)])
     if torch.cuda.is_available:
         model = model.cuda()
-    print(network)
+    print(model)
 
     # Extract deep dream image
     dreamed_image = deep_dream(
@@ -88,6 +112,7 @@ if __name__ == "__main__":
     os.makedirs("outputs", exist_ok=True)
     filename = args.input_image.split("/")[-1]
     plt.figure(figsize=(20, 20))
-    plt.imshow(dreamed_image)
-    plt.imsave(f"outputs/output_{filename}", dreamed_image)
+    res = deprocess(dreamed_image)
+    plt.imshow(res)
+    plt.imsave(f"outputs/output_{filename}", res)
     plt.show()
--- utils.py
+++ utils.py
@@ -5,13 +5,15 @@ from torchvision import transforms
 mean = np.array([0.485, 0.456, 0.406])
 std = np.array([0.229, 0.224, 0.225])
 
-preprocess = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean, std)])
+preprocess = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize(mean, std)]
+)
 
 
 def deprocess(image_np):
     image_np = image_np.squeeze().transpose(1, 2, 0)
     image_np = image_np * std.reshape((1, 1, 3)) + mean.reshape((1, 1, 3))
-    image_np = np.clip(image_np, 0.0, 255.0)
+    image_np = np.clip(image_np, 0.0, 1.0)
     return image_np
 
 
